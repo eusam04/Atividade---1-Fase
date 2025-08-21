c3628f3ee1b54279b23501d7e1679d10f923fcac
Samires do Carmo <samiresdocarmo@gmail.com>
2025-08-21 19:47:05 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,53 +4,58 @@ import java.util.Scanner;
 import Excecoes.QntdEstoqueInvalidaException;
 import Excecoes.CategoriaInvalidaException;
 import Excecoes.CodigoDuplicadoException;
+import excecoes.PrecoInvalidoException;
+import excecoes.EntradaInvalidaException;
+
 
 
 public class Main {
-	public static void main(String[]args) {
-		Sistema sistema=new Sistema();
-		Scanner sc=new Scanner (System.in);
-		int opcao;
-		 do {
-	            System.out.println("---Menu----");
-	            System.out.println("1 - Adicionar");
-	            System.out.println("2 - Listar");
-	            System.out.println("3 - Buscar");
-	            System.out.println("4 - Remover");
-	            System.out.println("5 - Sair");
-	            System.out.println("\nDigite a sua opção:");
-	            opcao = sc.nextInt();
-	            sc.nextLine();
+	public static void main(String[] args){
 		
-	            switch(opcao) {
-	            case 1:
-	                try {
-	                    sistema.adicionarProduto(sc);
-	                } catch (QntdEstoqueInvalidaException | CategoriaInvalidaException | CodigoDuplicadoException e) {
-	                    System.out.println("Erro ao adicionar produto: " + e.getMessage());
-	                }
-	                break;
-	    case 2:
-			sistema.listarProdutos();
-			break;
-		case 3:
-			sistema.buscarProduto(sc);
-			break;
-		case 4:
-		    try {
-		        sistema.removerProduto(sc);
-		    } catch (Exception e) {
-		        System.out.println("Erro ao remover produto: " + e.getMessage());
-		    }
-		    break;
-         default:
-             break;
-	    }
-	        } while (opcao != 5);
-		 	System.out.println("Saindo do Sistema!");
-
-	        sc.close();
-		 }
+		Sistema sistema = new Sistema();
+		Scanner scan = new Scanner(System.in);
+		
+		int opcao;
+		
+		do {
+			System.out.println("---Menu----");
+			System.out.println("1- Adicionar");
+			System.out.println("2- Listar");
+			System.out.println("3- Buscar");
+			System.out.println("4- Remover");
+			System.out.print("Digite sua opção: ");
+			opcao = scan.nextInt();
+			scan.nextLine();
+			
+			switch (opcao) {
+			case 1:
+				try {
+					sistema.adicionarProduto(scan);										
+				} catch (PrecoInvalidoException|EntradaInvalidaException e) {
+					System.out.println(e.getMessage());
+				}
+				break;
+			case 2:
+				sistema.listarProdutos();
+				break;
+			case 3:
+				sistema.buscarProduto(scan);
+				break;
+			case 4:
+				sistema.removerProduto(scan);
+				break;
+			default:
+				break;
+			}
+			
+		} while (opcao != 5);	
+		System.out.println("Saindo do Sistema!");
 	}
+}
+
+
+
+
+
 
 
